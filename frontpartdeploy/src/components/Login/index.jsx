9bb41cfe5c924d02f125cb9e45dd732a0ef0d0fc
Safal Chhetri <166716572+safaldev@users.noreply.github.com>
2024-06-21 10:18:43 +0545

    Update index.jsx

--- frontpartdeploy/src/components/Login/index.jsx
+++ frontpartdeploy/src/components/Login/index.jsx
@@ -14,7 +14,7 @@ const Login = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:8000/api/v1/login";
+			const url = "https://tribhuwan-admin.vercel.app/api/v1/login";
 			const { data: res } = await axios.post(url, data);
 			localStorage.setItem("token", res.data);
 			window.location = "/";
