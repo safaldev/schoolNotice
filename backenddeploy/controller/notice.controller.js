72e25990229d649d465dbe34c3da775d8bf233a3
Safal Chhetri <166716572+safaldev@users.noreply.github.com>
2024-06-21 10:05:35 +0545

    V1
    Deployed Backend

---
+++ backenddeploy/controller/notice.controller.js
@@ -0,0 +1,29 @@
+import { Notice } from '../models/notice.models.js'
+import { ApiError } from '../utils/ApiError.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+
+const addNotice = asyncHandler(async (req, res) => {
+
+const {title, message} = req.body;
+
+const existedNotice = await Notice.findOne({
+    $or: [{ title }],
+});
+
+if (existedNotice) {
+    throw new ApiError(409, "User with email or username already exists");
+}
+
+const notice = await Notice.create({
+    title,
+    message,
+});
+
+
+const createdNotice = await Notice.findById(notice._id).select();
+
+console.log(createdNotice);
+})
+
+
+export { addNotice };
