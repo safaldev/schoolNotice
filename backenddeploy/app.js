1c84ce5fa58c068716392e2d003b10bb9a087a99
Safal Chhetri <166716572+safaldev@users.noreply.github.com>
2024-06-22 21:15:58 +0545

    Update app.js

--- backenddeploy/app.js
+++ backenddeploy/app.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/v1/noticeData', (req, res) => {
-            dataCollector()
+            // dataCollector()
             res.send(noticeData)
 })
 
